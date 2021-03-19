b8d783d6b27f9cfaeacfbd12bbd755f1aeaee381
Byungwoo Kim <rlaquddn05@naver.com>
2021-03-19 13:36:10 +0900

    dev config

--- src/main/java/toeicLab/toeicLab/configuration/DevConfiguration.java
+++ src/main/java/toeicLab/toeicLab/configuration/DevConfiguration.java
@@ -5,9 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import toeicLab.toeicLab.domain.Member;
-import toeicLab.toeicLab.domain.MemberType;
+import toeicLab.toeicLab.domain.*;
 import toeicLab.toeicLab.repository.MemberRepository;
+import toeicLab.toeicLab.repository.StudyGroupApplicationRepository;
+import toeicLab.toeicLab.repository.StudyGroupRepository;
 
 import javax.annotation.PostConstruct;
 
@@ -19,16 +20,26 @@ public class DevConfiguration {
 
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
+    private final StudyGroupRepository studyGroupRepository;
+    private final StudyGroupApplicationRepository studyGroupApplicationRepository;
+    private final int NUMBER_OF_DUMMY_USERS = 1000;
+    private final LevelType[] levelTypes = {LevelType.BEGINNER,
+            LevelType.INTERMEDIATE, LevelType.ADVANCED};
+    private final GenderType[] genderTypes = {GenderType.MALE, GenderType.FEMALE};
 
     @PostConstruct
-    public void createTestUser(){
-        Member member = Member.builder()
-                .email("a@a.a")
-                .password(passwordEncoder.encode("1234"))
-                .memberType(MemberType.USER)
-                .build();
-
-        memberRepository.save(member);
-        log.info("TestUser (a@a.a) has been created.");
+    public void createDummyUsers() {
+        for (int i = 1; i <= NUMBER_OF_DUMMY_USERS; i++) {
+            Member member = Member.builder()
+                    .email("dummy" + i + "@a.a")
+                    .password(passwordEncoder.encode("1234"))
+                    .memberType(MemberType.USER)
+                    .levelType(levelTypes[(int)(Math.random()*3+1)])
+                    .age((int)(Math.random()*25+10))
+                    .genderType(genderTypes[(int)(Math.random()*2+1)])
+                    .build();
+            memberRepository.save(member);
+        }
+        log.info("DummyUsers created.");
     }
 }
