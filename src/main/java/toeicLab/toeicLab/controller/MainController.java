add883471b687a0705ab530ee1628d775667d1cc
Hyeonseong Lee <zzzdlgustjd@gmail.com>
2021-03-23 12:04:51 +0900

    오류 수정

--- src/main/java/toeicLab/toeicLab/controller/MainController.java
+++ src/main/java/toeicLab/toeicLab/controller/MainController.java
@@ -7,8 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import toeicLab.toeicLab.domain.MailDto;
 import toeicLab.toeicLab.domain.Member;
 import toeicLab.toeicLab.repository.MailRepository;
@@ -24,8 +23,6 @@ import toeicLab.toeicLab.user.SignUpForm;
 import toeicLab.toeicLab.user.SignUpValidator;
 
 import javax.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -40,25 +37,25 @@ public class MainController {
     private final QuestionSetService questionSetService;
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "/view/index";
     }
 
     @GetMapping("/login")
-    public String showLoginPage(){
+    public String showLoginPage() {
         return "/view/login";
     }
 
     @GetMapping("/send_reset_password_link")
-    public String sendResetPasswordView(){
+    public String sendResetPasswordView() {
         return "/view/send_reset_password_link";
     }
 
     @PostMapping("/send_reset_password_link")
     public String sendResetPassword(String userId, String email, Model model) {
-        try{
+        try {
             memberService.sendResetPasswordEmail(userId, email);
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             model.addAttribute("error_code", "password.reset.failed");
             return "/view/notify_password";
         }
@@ -68,12 +65,12 @@ public class MainController {
     }
 
     @GetMapping("reset_password")
-    public String resetPasswordView(String email, String emailCheckToken, Model model){
+    public String resetPasswordView(String email, String emailCheckToken, Model model) {
         boolean result = memberService.checkEmailToken(email, emailCheckToken);
 
-        if(result){
+        if (result) {
             model.addAttribute("result", true);
-        }else{
+        } else {
             model.addAttribute("result", false);
         }
 
@@ -83,22 +80,22 @@ public class MainController {
     }
 
     @PostMapping("reset_password")
-    public String resetPassword(String email, String password, Model model){
+    public String resetPassword(String email, String password, Model model) {
         memberService.resetPassword(email, password);
         model.addAttribute("result_code", "password.reset.success");
         return "/view/notify_password";
     }
 
     @GetMapping("/send_find_id_link")
-    public String sendFindIdView(){
+    public String sendFindIdView() {
         return "/view/send_find_id_link";
     }
 
     @PostMapping("/send_find_id_link")
     public String sendFindId(String email, Model model) {
-        try{
+        try {
             memberService.sendFindIdByEmail(email);
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             model.addAttribute("error_code", "find.id.failed");
             return "/view_find_id";
         }
@@ -107,15 +104,15 @@ public class MainController {
     }
 
     @GetMapping("/signup")
-    public String signup(Model model){
+    public String signup(Model model) {
         model.addAttribute(new SignUpForm());
         return "/view/signup";
     }
 
     @PostMapping("/signup")
-    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
         log.info("하...");
-        if(errors.hasErrors()){
+        if (errors.hasErrors()) {
             log.info("유효성 에러 발생!");
             return "/view/signup";
         }
@@ -140,16 +137,16 @@ public class MainController {
 
     @GetMapping("/checkTokens")
     @ResponseBody
-    public String checkTokens(@RequestParam("token") String token, @RequestParam("inputToken") String inputToken){
+    public String checkTokens(@RequestParam("token") String token, @RequestParam("inputToken") String inputToken) {
         JsonObject jsonObject = new JsonObject();
 
         boolean result = false;
 
         result = token.equals(inputToken);
 
-        if(result){
+        if (result) {
             jsonObject.addProperty("message", "이메일 인증 성공");
-        }else {
+        } else {
             jsonObject.addProperty("message", "이메일 인증 실패");
         }
         return jsonObject.toString();
@@ -157,16 +154,16 @@ public class MainController {
 
     @GetMapping("/checkUserId")
     @ResponseBody
-    public String checkUserId(@RequestParam("userId") String userId){
+    public String checkUserId(@RequestParam("userId") String userId) {
         JsonObject jsonObject = new JsonObject();
 
         boolean result = false;
 
         result = memberRepository.existsByUserId(userId);
 
-        if(result){
+        if (result) {
             jsonObject.addProperty("message", "이미 존재하는 아이디입니다.");
-        }else {
+        } else {
             jsonObject.addProperty("message", "사용 가능한 아이디입니다.");
         }
         return jsonObject.toString();
@@ -174,16 +171,16 @@ public class MainController {
 
     @GetMapping("/checkNickname")
     @ResponseBody
-    public String checkNickname(@RequestParam("nickname") String nickname){
+    public String checkNickname(@RequestParam("nickname") String nickname) {
         JsonObject jsonObject = new JsonObject();
 
         boolean result = false;
 
         result = memberRepository.existsByNickname(nickname);
 
-        if(result){
+        if (result) {
             jsonObject.addProperty("message", "이미 존재하는 닉네임입니다.");
-        }else {
+        } else {
             jsonObject.addProperty("message", "사용 가능한 닉네임입니다.");
         }
         return jsonObject.toString();
@@ -191,163 +188,163 @@ public class MainController {
 
     @GetMapping("/checkPasswords")
     @ResponseBody
-    public String checkPasswords(@RequestParam("password") String password, @RequestParam("check_password") String check_password){
+    public String checkPasswords(@RequestParam("password") String password, @RequestParam("check_password") String check_password) {
         JsonObject jsonObject = new JsonObject();
 
         boolean result = false;
 
         result = password.equals(check_password);
 
-        if(result){
+        if (result) {
             jsonObject.addProperty("message", "비밀번호가 일치합니다,");
-        }else {
+        } else {
             jsonObject.addProperty("message", "비밀번호가 일치하지 않습니다.");
         }
         return jsonObject.toString();
     }
 
     @GetMapping("/my_page")
-    public String myPage(){
+    public String myPage() {
         return "/view/my_page";
     }
 
     @GetMapping("/my_progress")
-    public String myProgress(){
+    public String myProgress() {
         return "/view/my_progress";
     }
 
     @GetMapping("/my_studygroup_list")
-    public String myStudyGroupList(){
+    public String myStudyGroupList() {
         return "/view/my_studygroup_list";
     }
 
     @GetMapping("/my_studygroup")
-    public String myStudyGroup(){
+    public String myStudyGroup() {
         return "/view/my_studygroup";
     }
 
     @GetMapping("/create_meeting")
-    public String createMeeting(){
+    public String createMeeting() {
         return "/view/create_meeting";
     }
 
     @GetMapping("/my_vocabulary_list")
-    public String myVocabularyList(){
+    public String myVocabularyList() {
         return "/view/my_vocabulary_list";
     }
 
     @GetMapping("/toeiclab_introduction")
-    public String toeiclabIntroduction(){
+    public String toeiclabIntroduction() {
         return "/view/toeiclab_intro";
     }
 
     @GetMapping("/select_test")
-    public String selectTest(){
+    public String selectTest() {
         return "/view/select_test";
     }
 
     @GetMapping("/rc_sheet")
-    public String rcSheet(){
+    public String rcSheet() {
         return "/view/rc_sheet";
     }
 
     @GetMapping("/lc_sheet")
-    public String lcSheet(){
+    public String lcSheet() {
         return "/view/lc_sheet";
     }
 
     @GetMapping("/spk_sheet")
-    public String spkSheet(){
+    public String spkSheet() {
         return "/view/spk_sheet";
     }
 
     @GetMapping("/spk_confirm_answer")
-    public String spkConfirmAnswer(){
+    public String spkConfirmAnswer() {
         return "/view/spk_confirm_answer";
     }
 
     @GetMapping("/apply_studygroup")
-    public String applyStudygroup(){
+    public String applyStudygroup() {
         return "/view/apply_studygroup";
     }
 
     @GetMapping("/result_sheet")
     @PostMapping("/result_sheet")
-    public String resultSheet(){
+    public String resultSheet() {
         return "/view/result_sheet";
     }
 
     @GetMapping("/my_review_note")
-    public String myReviewNote(){
+    public String myReviewNote() {
         return "/view/my_review_note";
     }
 
     @GetMapping("/rc_answer_sheet")
-    public String rcAnswerSheet(){
+    public String rcAnswerSheet() {
         return "/view/rc_answer_sheet";
     }
 
     @GetMapping("/lc_answer_sheet")
-    public String lcAnswerSheet(){
+    public String lcAnswerSheet() {
         return "/view/lc_answer_sheet";
     }
 
     @GetMapping("/spk_answer_sheet")
-    public String spkAnswerSheet(){
+    public String spkAnswerSheet() {
         return "/view/spk_answer_sheet";
     }
 
     @GetMapping("/vocabulary_test")
-    public String vocabularyTest(){
+    public String vocabularyTest() {
         return "/view/vocabulary_test";
     }
 
     @PostMapping("/popup_dictionary")
-    public String popupDictionary(){
+    public String popupDictionary() {
         return "/view/popup_dictionary";
     }
 
     @GetMapping("/forum")
-    public String forum(){
+    public String forum() {
         return "/view/forum";
     }
 
     @GetMapping("/schedule")
-    public String schedule(){
+    public String schedule() {
         return "/view/schedule";
     }
 
-    @GetMapping("/practice_test")
-    public String practiceTest(){
-    @GetMapping("/practice_test/{id}")
-    public String practiceTest(@PathVariable String id, Model model){
-        System.out.println(id);
-        return "/view/practice_test";
-    }
 
-    @GetMapping("/test/{type}")
-    @Transactional
-    public String test1(@CurrentUser Member member, @PathVariable String type, Model model){
-        QuestionSet list = new QuestionSet();
-        switch (type){
-            case "quarter":
-                list = questionSetService.createToeicSet(QuestionSetType.QUARTER_TOEIC);
-                break;
-
-            case "half":
-                list = questionSetService.createToeicSet(QuestionSetType.HALF_TOEIC);
-                break;
-
-
-            case "full":
-                list = questionSetService.createToeicSet(QuestionSetType.FULL_TOEIC);
-                break;
-            default:
-
-                break;
+        @GetMapping("/practice_test/{id}")
+        public String practiceTest (@PathVariable String id, Model model){
+            System.out.println(id);
+            return "/view/practice_test";
         }
-        model.addAttribute("QuestionList", list.getQuestions());
-        return "/view/question/test";
-    }
 
-}
+
+        @GetMapping("/test/{type}")
+        @Transactional
+        public String test1 (@CurrentUser Member member, @PathVariable String type, Model model){
+            QuestionSet list = new QuestionSet();
+            switch (type) {
+                case "quarter":
+                    list = questionSetService.createToeicSet(QuestionSetType.QUARTER_TOEIC);
+                    break;
+
+                case "half":
+                    list = questionSetService.createToeicSet(QuestionSetType.HALF_TOEIC);
+                    break;
+
+
+                case "full":
+                    list = questionSetService.createToeicSet(QuestionSetType.FULL_TOEIC);
+                    break;
+                default:
+
+                    break;
+            }
+            model.addAttribute("QuestionList", list.getQuestions());
+            return "/view/question/test";
+        }
+
+    }
