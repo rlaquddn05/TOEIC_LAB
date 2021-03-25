b8e22bbc8bc56f7a1631fcb16b62c7f39d8eecc8
Byungwoo Kim <rlaquddn05@naver.com>
2021-03-25 14:31:54 +0900

    1. dev configure testuser >> testUser 2. 스터디그룹 신청기능

---
+++ src/main/java/toeicLab/toeicLab/user/StudyGroupApplicationForm.java
@@ -0,0 +1,45 @@
+package toeicLab.toeicLab.user;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.lang.NonNull;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class SignUpForm {
+
+    @NotBlank
+    private String userId;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String check_password;
+
+    @NotBlank
+    private String nickname;
+
+    @NotBlank
+    private String gender;
+
+    @NotNull
+    private int age;
+
+    @NotBlank
+    @Length(min = 5, max=40)
+    @Email
+    private String email;
+
+    @NotBlank
+    private String contact;
+
+    @NotBlank
+    private String zipcode;
+    private String city;
+    private String street;
+
+}
