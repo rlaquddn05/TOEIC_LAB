8cdda38dd12de4669654602998d27a3eff4730a2
Hyeonseong Lee <zzzdlgustjd@gmail.com>
2021-04-21 19:30:40 +0900

    front 쪽 끝냄

--- src/main/resources/static/js/group/unused.js
+++ src/main/resources/static/js/group/unused.js
@@ -78,3 +78,19 @@ window.onload = function() {
         })
     }
 }
+
+window.onload = function () {
+    var error = [[${errorMessage}]];
+    var success = [[${successMessage}]];
+    if (error != null) {
+        $('.modal-body').html(error);
+        $('#errorModal').modal('show');
+    }
+    if (success != null) {
+        $('.modal-body').html(success);
+        $('#exampleModalCenter').modal('show');
+        $('#exampleModalCenter').on('hidden.bs.modal', function () {
+            location.href = '/'
+        })
+    }
+}
